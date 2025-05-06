cfe9df95a18ec39aecc81dafdc0e714ee9b5a142
Ruben Manuel <rubensapalo97@gmail.com>
2025-05-06 02:43:08 +0100

    first commit

---
+++ shop_catalogo/src/main/java/com/rudev/shop_catalogo/ShopCatalogoApplication.java
@@ -0,0 +1,13 @@
+package com.rudev.shop_catalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopCatalogoApplication.class, args);
+	}
+
+}
---
+++ shop_catalogo/src/test/java/com/rudev/shop_catalogo/ShopCatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudev.shop_catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
