81fc168a5df6583bddbad2e9761b66d1bca326eb
Ruben Manuel <rubensapalo97@gmail.com>
2025-05-09 09:13:56 +0100

    Dados de Auditoria

--- shop_catalogo/src/main/java/com/rudev/shop_catalogo/entities/Category.java
+++ shop_catalogo/src/main/java/com/rudev/shop_catalogo/entities/Category.java
@@ -2,8 +2,13 @@ package com.rudev.shop_catalogo.entities;
 
 import java.util.Objects;
 import java.io.Serializable;
+import java.time.Instant;
+
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +22,11 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
 	
 	public Category() {
 	}
@@ -40,6 +50,24 @@ public class Category implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
 	}
 
 	@Override
