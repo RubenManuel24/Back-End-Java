d596a443f6f6b1be41c59e62364991fa9d67061a
Ruben Manuel <rubensapalo97@gmail.com>
2025-06-15 00:16:33 +0100

    Teste Unitário - findAllPage

--- shop_catalogo/src/test/java/com/rudev/shop_catalogo/services/ProductServiceTests.java
+++ shop_catalogo/src/test/java/com/rudev/shop_catalogo/services/ProductServiceTests.java
@@ -1,17 +1,25 @@
 package com.rudev.shop_catalogo.services;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatcher;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.rudev.shop_catalogo.entities.Product;
 import com.rudev.shop_catalogo.repositories.CategoryRepository;
 import com.rudev.shop_catalogo.repositories.ProductRepository;
+import com.rudev.shop_catalogo.tests.Factory;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -28,12 +36,18 @@ public class ProductServiceTests {
 	 private long existedId;
 	 private long notExistedId;
 	 private long dependentId;
+	 private PageImpl<Product> page;
+	 private Product product;
 	 
 	 @BeforeEach
 	 void setUp() throws Exception{
 		 existedId = 1L;
 		 notExistedId = 2L;
 		 dependentId = 3L;
+		 product = Factory.createProduct();
+		 page = new PageImpl<>(List.of(product));
+		 
+		 Mockito.when(productRepository.findAll((Pageable)ArgumentMatchers.any())).thenReturn(page);
 		 
 		 Mockito.doNothing().when(productRepository).deleteById(existedId);
 		 Mockito.when(productRepository.existsById(existedId)).thenReturn(true);
